39d23e6756998886b18cdd3ba8448596e533867c
minyoung22222 <alsdudr97@naver.com>
2022-06-10 03:39:02 +0900

    colaBtns click event update

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const colaList = document.querySelectorAll('.cola_btn');
+const colaBtns = document.querySelectorAll('.cola_btn');
 const selectList = document.querySelector('.select_cola_list');
 
-colaList.forEach(element => {
-    element.addEventListener('click', () => {
-            console.log('cola click!');
-            const selectLi = document.createElement('li');
-            selectList.append(`
-            <li><img src="mediaquery/Original_Cola.svg" alt="">
-            <span>Original_Cola</span>
-            <button class="count">1</button></li>
-            `
-            );
+
+colaBtns.forEach(colaBtn => {
+    colaBtn.addEventListener('click', (e) => {
+        const selectedCola = e.target;
+        
+        const createLi = document.createElement('li');
+
+        const colaLiImg = selectedCola.querySelector('img');
+        const colaImgsrc = colaLiImg.getAttribute('src');
+        const createLiImg = document.createElement('img');
+        createLiImg.setAttribute('src', colaImgsrc);
+
+        const createStrong = document.createElement('strong');
+        createStrong.classList.add('cola_name');
+        createStrong.append(selectedCola.querySelector('.cola_name').textContent);
+
+        const createBtn = document.createElement('span');
+        createBtn.classList.add('count')
+
+        createLi.append(createLiImg, createStrong, createBtn)
+        
+        selectList.append(createLi)
         
     });
 });
